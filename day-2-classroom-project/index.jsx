a26c0f1012bcd80a8f8a128d2b6131041688e68a
Bhagwat dixit <bhagwatdixit55@gmail.com>
2024-04-29 11:04:47 +0530

    classroom 2 work

---
+++ day-2-classroom-project/index.jsx
@@ -0,0 +1,34 @@
+const MainContainer = () =>{
+  return (
+    <div id="box">
+      <h1>Learn Web Development</h1>
+
+      <div className="div1">
+
+     
+      <p>Welcome to MDN learning area. adipisicing elit. Nobis laborum, fugiat impedit repellendus distinctio ratione asperiores veniam. Nulla, nesciunt voluptatibus!</p>
+
+     
+      </div>
+
+
+    <div className="div2">
+     
+
+      <p>Html Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsam minima <span>the rest of MDN</span> possimus nemo reprehenderit maiores totam voluptas nulla dolorum soluta.</p>
+</div>
+      <div className="div3">
+     
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia obcaecati quis ducimus fuga quos. Consectetur voluptas natus rerum ipsam tempora porro praesentium, quidem inventore molestias?</p>
+    </div>
+    </div>
+  )
+}
+
+
+
+
+
+
+ReactDOM.render(<MainContainer/>, document.querySelector("#root"))
+
